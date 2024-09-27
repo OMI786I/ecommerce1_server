d211df5250246d28f852adcf74c473a3cbb098f2
OMI786I <sohrawardy1998@gmail.com>
2024-09-27 22:52:18 +0600

    get operation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -9,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://<db_username>:<db_password>@cluster0.ymyoldm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ymyoldm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,6 +23,14 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const shoeCollection = client.db("ecommerce1").collection("shoes");
+
+    app.get("/shoes", async (req, res) => {
+      const cursor = shoeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // // Send a ping to confirm a successful connection
