86de7ecde73dc7b207c2b2a444be96ded7dc04c7
OMI786I <sohrawardy1998@gmail.com>
2024-09-30 22:31:53 +0600

    getting data according to shoe id

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ymyoldm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,6 +33,14 @@ async function run() {
       }
       const cursor = shoeCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/shoes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await shoeCollection.findOne(query);
+
       res.send(result);
     });
 
